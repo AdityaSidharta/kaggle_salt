f2f18907c258e2031b9b95a991be8c71e635a0c4
AdityaSidharta <adityasidharta@github.com>
2018-07-26 14:55:15 +0800

    Add CRF, image classifier

---
+++ python/crf.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pydensecrf.densecrf as dcrf
+from skimage.io import imread, imsave
+from pydensecrf.utils import unary_from_labels, create_pairwise_bilateral
+from skimage.color import gray2rgb
+from skimage.color import rgb2gray
+import matplotlib.pyplot as plt
+import pandas as pd
+from tqdm import tqdm
+
+def rle_decode(rle_mask):
+    '''
+    rle_mask: run-length as string formated (start length)
+    shape: (height,width) of array to return
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = rle_mask.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(101*101, dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(101,101)
+
+def rle_encode(im):
+    '''
+    im: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = im.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+def crf(original_image, mask_img):
+    if (len(mask_img.shape) < 3):
+        mask_img = gray2rgb(mask_img)
+    annotated_label = mask_img[:, :, 0] + (mask_img[:, :, 1] << 8) + (mask_img[:, :, 2] << 16)
+    colors, labels = np.unique(annotated_label, return_inverse=True)
+    n_labels = 2
+    d = dcrf.DenseCRF2D(original_image.shape[1], original_image.shape[0], n_labels)
+    U = unary_from_labels(labels, n_labels, gt_prob=0.7, zero_unsure=False)
+    d.setUnaryEnergy(U)
+    d.addPairwiseGaussian(sxy=(3, 3), compat=3, kernel=dcrf.DIAG_KERNEL,
+                          normalization=dcrf.NORMALIZE_SYMMETRIC)
+    Q = d.inference(10)
+    MAP = np.argmax(Q, axis=0)
+    return MAP.reshape((original_image.shape[0], original_image.shape[1]))
+
--- python/models_utils.py
+++ python/models_utils.py
@@ -1,18 +1,62 @@
 from keras import layers
 import keras.backend as K
-from keras.layers import Conv2D, BatchNormalization, Activation
+from keras.layers import Conv2D, BatchNormalization, Activation, MaxPooling2D, Conv2DTranspose, concatenate, Dropout, Dense
 
-def identity_block(input_tensor, kernel_size, filters, stage, block):
-    """The identity block is the block that has no conv layer at shortcut.
-    # Arguments
-        input_tensor: input tensor
-        kernel_size: defualt 3, the kernel size of middle conv layer at main path
-        filters: list of integers, the filterss of 3 conv layer at main path
-        stage: integer, current stage label, used for generating layer names
-        block: 'a','b'..., current block label, used for generating layer names
-    # Returns
-        Output tensor for the block.
-    """
+def conv_block(inp, filter_size, is_maxpool = True):
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(inp)
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(c)
+    if is_maxpool:
+        p = MaxPooling2D((2, 2))(c)
+        return c, p
+    else:
+        return c
+
+def trans_conv_block(inp, c_inp, filter_size):
+    u = Conv2DTranspose(filter_size, (2, 2), strides=(2, 2), padding='same')(inp)
+    u = concatenate([u, c_inp])
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(u)
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(c)
+    return c
+
+def dense_block_bn_dp(x, dense_list, dropout = 0.0):
+    dense_list = [dense_list] if type(dense_list) != list else dense_list
+    for dense_size in dense_list:
+        x = Dense(dense_size, activation='relu')(x)
+        x = BatchNormalization()(x)
+        x = Dropout(dropout)(x)
+    return x
+
+def conv_block_bn_dp(inp, filter_size, dropout, is_maxpool = True):
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(inp)
+    c = BatchNormalization()(c)
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(c)
+    c = BatchNormalization()(c)
+    if is_maxpool:
+        p = MaxPooling2D((2, 2))(c)
+        p = Dropout(dropout)(p)
+        return c, p
+    else:
+        c = Dropout(dropout)(c)
+        return c
+
+def trans_conv_block_bn_dp(inp, c_inp, filter_size, dropout):
+    u = Conv2DTranspose(filter_size, (2, 2), strides=(2, 2), padding='same')(inp)
+    u = concatenate([u, c_inp])
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(u)
+    c = BatchNormalization()(c)
+    c = Conv2D(filter_size, (3, 3), activation='relu', padding='same')(c)
+    c = BatchNormalization()(c)
+    c = Dropout(dropout)(c)
+    return c
+
+def res_trans_conv_block_bn_dp(inp, c_inp, filter_size, dropout):
+    u = Conv2DTranspose(filter_size, (2, 2), strides=(2, 2), padding='same')(inp)
+    u = BatchNormalization()(u)
+    u = concatenate([u, c_inp])
+    u = Dropout(dropout)(u)
+    return u
+
+def res_identity_block(input_tensor, kernel_size, filters, stage, block, dropout = 0.0):
     filters1, filters2, filters3 = filters
     if K.image_data_format() == 'channels_last':
         bn_axis = 3
@@ -21,35 +65,26 @@ def identity_block(input_tensor, kernel_size, filters, stage, block):
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    x = Conv2D(filters1, (1, 1), name=conv_name_base + '2a')(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Conv2D(filters1, (1, 1), name=conv_name_base + 'a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + 'a')(x)
+    x = Dropout(dropout)(x)
     x = Activation('relu')(x)
 
     x = Conv2D(filters2, kernel_size,
-               padding='same', name=conv_name_base + '2b')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+               padding='same', name=conv_name_base + 'b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + 'b')(x)
+    x = Dropout(dropout)(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + 'c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + 'c')(x)
+    x = Dropout(dropout)(x)
+    x = Activation('relu')(x)
 
     x = layers.add([x, input_tensor])
-    x = Activation('relu')(x)
     return x
 
-def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
-    """conv_block is the block that has a conv layer at shortcut
-    # Arguments
-        input_tensor: input tensor
-        kernel_size: defualt 3, the kernel size of middle conv layer at main path
-        filters: list of integers, the filterss of 3 conv layer at main path
-        stage: integer, current stage label, used for generating layer names
-        block: 'a','b'..., current block label, used for generating layer names
-    # Returns
-        Output tensor for the block.
-    Note that from stage 3, the first conv layer at main path is with strides=(2,2)
-    And the shortcut should have strides=(2,2) as well
-    """
+def res_conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2), dropout = 0.0):
     filters1, filters2, filters3 = filters
     if K.image_data_format() == 'channels_last':
         bn_axis = 3
@@ -59,22 +94,27 @@ def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2))
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
     x = Conv2D(filters1, (1, 1), strides=strides,
-               name=conv_name_base + '2a')(input_tensor)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+               name=conv_name_base + 'a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + 'a')(x)
+    x = Dropout(dropout)(x)
     x = Activation('relu')(x)
 
     x = Conv2D(filters2, kernel_size, padding='same',
-               name=conv_name_base + '2b')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+               name=conv_name_base + 'b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + 'b')(x)
+    x = Dropout(dropout)(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(filters3, (1, 1), name=conv_name_base + '2c')(x)
-    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+    x = Conv2D(filters3, (1, 1), name=conv_name_base + 'c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + 'c')(x)
+    x = Dropout(dropout)(x)
+    x = Activation('relu')(x)
 
     shortcut = Conv2D(filters3, (1, 1), strides=strides,
                       name=conv_name_base + '1')(input_tensor)
     shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+    shortcut = Dropout(dropout)(shortcut)
+    shortcut = Activation('relu')(shortcut)
 
     x = layers.add([x, shortcut])
-    x = Activation('relu')(x)
     return x
